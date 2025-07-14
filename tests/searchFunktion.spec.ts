d46ab5c8600ba7bf09c90e2528695dc7f33ba00b
master5100125 <fb.smirnov.u@gmail.com>
2025-07-14 13:36:53 +0300

    Додав домашню роботу по елементу search

--- tests/searchFunktion.spec.ts
+++ tests/searchFunktion.spec.ts
@@ -13,8 +13,20 @@ test.describe('Check funktion serching', () => {
 
   test('check exist element', async ({ page }) => {
     const searchInput = page.locator('input[name="q"]');
+    const searchAmoutResults = page
+      .locator('div.Segment__Title span.Heading.Text--subdued.u-h7')
+      .filter({ hasText: /results$/ }); //довго мучався, бо локатор не унікальний і який би не робив локатор находило 2 елемента. Цей підхід підсказав gpt
+    const searhResult = page.locator('div.Grid__Cell').filter({ hasText: /Smart Temperature/ });
+    const searchClose = page.locator('button.Search__Close');
 
-    await searchInput.fill('');
+    await searchInput.fill('Smart Temperature');
+    await searchAmoutResults.isVisible();
+    await expect(searchAmoutResults).toHaveText('15 results');
+    await expect(searhResult).toHaveCount(3);
+    await searchClose.click();
+    await page.waitForTimeout(1000);
+    await expect(page.locator('div.Search_Inner')).not.toBeVisible();
+
     //написати тест на пошук по тексту та перевірку тих товарів які будуть виведені в результаті. Також перевірити кількість, ну короч по красивому
   });
 });
