75b51a158cd9289a2772f5ef6d8523406d02dfba
master5100125 <fb.smirnov.u@gmail.com>
2025-07-22 11:57:30 +0300

    Додав перевірку 'expires'

--- tests/cookies.spec.ts
+++ tests/cookies.spec.ts
@@ -7,27 +7,37 @@ test.describe('Check cookies', () => {
 
   test('Check localization cookie', async ({ context }) => {
     const cookies = await context.cookies();
-    // console.log(cookies);
+    console.log(cookies);
     const localization = cookies.find((c) => c.name === 'localization');
-    await expect(localization).toBeDefined();
+    const now = Date.now() / 1000;
+    const minExpireData = now + 60 * 60 * 24 * 364; // 60 секунд і 60 хвилин, 24 години на день, 363 днів (похибка на мінус 1 день)
+    const maxExpireData = now + 60 * 60 * 24 * 366; //а тут похибка на +1 день
 
+    await expect(localization).toBeDefined();
     await expect(localization?.value).toBe('UA');
     await expect(localization?.domain).toBe('theconnectedshop.com');
     await expect(localization?.path).toBe('/');
     await expect(localization?.httpOnly).toBe(false);
     await expect(localization?.secure).toBe(false);
+    await expect(localization?.expires).toBeGreaterThan(minExpireData);
+    await expect(localization?.expires).toBeLessThan(maxExpireData);
   });
 
   test('Check currency cookie', async ({ context }) => {
     const cookies = await context.cookies();
     const currencyCart = cookies.find((c) => c.name === 'cart_currency');
     // console.log(currencyCart);
-    await expect(currencyCart).toBeDefined();
+    const now = Date.now() / 1000;
+    const minExpireData = now + 60 * 60 * 24 * 13; // 60 секунд і 60 хвилин, 24 години на день, 363 днів (похибка на мінус 1 день)
+    const maxExpireData = now + 60 * 60 * 24 * 15; //а тут похибка на +1 день
 
+    await expect(currencyCart).toBeDefined();
     await expect(currencyCart?.value).toBe('UAH');
     await expect(currencyCart?.domain).toBe('theconnectedshop.com');
     await expect(currencyCart?.path).toBe('/');
     await expect(currencyCart?.httpOnly).toBe(false);
     await expect(currencyCart?.secure).toBe(false);
+    await expect(currencyCart?.expires).toBeGreaterThan(minExpireData);
+    await expect(currencyCart?.expires).toBeLessThan(maxExpireData);
   });
 });
