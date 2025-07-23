bb74046953ff7c23c2552eefa8f116f0e6de5561
master5100125 <fb.smirnov.u@gmail.com>
2025-07-23 16:46:44 +0300

    Переписана структура

--- tests/UI_old/searchFunction.spec.ts
+++ tests/UI_old/searchFunction.spec.ts
@@ -1,45 +1,54 @@
 import { test, expect } from '@playwright/test';
+import { Search } from '../pages/search';
 
 test.describe('Check function serching', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto('/');
-    const searchLink = page.locator('#section-header').getByText('Search');
-    await expect(searchLink).toBeVisible();
-    await searchLink.click();
-  });
+  // test.beforeEach(async ({ page }) => {
+  //   await page.goto('/');
+  //   const searchLink = page.locator('#section-header').getByText('Search');
+  //   await expect(searchLink).toBeVisible();
+  //   await searchLink.click();
+  // });
 
-  test.afterEach(async ({ page }) => {
-    await page.close();
-  });
+  // test.afterEach(async ({ page }) => {
+  //   await page.close();
+  // });
 
   test('check exist element', async ({ page }) => {
-    const searchInput = page.locator('input[name="q"]');
-    const searchAmoutResults = page
-      .locator('div.Segment__Title span.Heading.Text--subdued.u-h7')
-      .filter({ hasText: /results$/ }); //довго мучався, бо локатор не унікальний і який би не робив локатор находило 2 елемента. Цей підхід підсказав gpt
-    const searchClose = page.locator('button.Search__Close');
-    const firstProductTitle = page.locator('h2.ProductItem__Title a').first(); //Перший елемент по рахунку, якщо найдено по локатору 2
+    const search = new Search(page);
+    await search.openSearchPanel();
     const query = 'Smart water Temperature sensor';
 
-    await expect(searchInput).toHaveAttribute('placeholder', 'Search...');
-    await expect(searchInput).toHaveAttribute('type', 'search');
-    await expect(searchInput).toHaveAttribute('aria-label', 'Search...');
+    await search.assertSearchInputAttributes();
+    await search.searchFill(query);
+    await search.assertResultAmount();
+    await search.assertFirstProductContains(query);
+    // const searchInput = page.locator('input[name="q"]');
+    // const searchAmoutResults = page
+    //   .locator('div.Segment__Title span.Heading.Text--subdued.u-h7')
+    //   .filter({ hasText: /results$/ }); //довго мучався, бо локатор не унікальний і який би не робив локатор находило 2 елемента. Цей підхід підсказав gpt
+    // const searchClose = page.locator('button.Search__Close');
+    // const firstProductTitle = page.locator('h2.ProductItem__Title a').first(); //Перший елемент по рахунку, якщо найдено по локатору 2
+    // const query = 'Smart water Temperature sensor';
 
-    await searchInput.fill(query);
-    await expect(searchInput).toHaveValue(query);
+    // await expect(searchInput).toHaveAttribute('placeholder', 'Search...');
+    // await expect(searchInput).toHaveAttribute('type', 'search');
+    // await expect(searchInput).toHaveAttribute('aria-label', 'Search...');
 
-    await expect(searchAmoutResults).toBeVisible();
+    // await searchInput.fill(query);
+    // await expect(searchInput).toHaveValue(query);
 
-    //Далі перевіряємо результат що поле "Results не 0"
-    const resultText = await searchAmoutResults.textContent();
-    const match = resultText?.match(/(\d+)\s+results/);
-    await expect(match).not.toBeNull();
-    const resultCount = Number(match![1]);
-    await expect(resultCount).toBeGreaterThan(0);
+    // await expect(searchAmoutResults).toBeVisible();
 
-    const productText = await firstProductTitle.textContent();
-    await expect(productText?.toLowerCase()).toContain(query.toLowerCase());
-    await searchClose.click();
-    await expect(searchInput).not.toBeVisible();
+    // //Далі перевіряємо результат що поле "Results не 0"
+    // const resultText = await searchAmoutResults.textContent();
+    // const match = resultText?.match(/(\d+)\s+results/);
+    // await expect(match).not.toBeNull();
+    // const resultCount = Number(match![1]);
+    // await expect(resultCount).toBeGreaterThan(0);
+
+    // const productText = await firstProductTitle.textContent();
+    // await expect(productText?.toLowerCase()).toContain(query.toLowerCase());
+    // await searchClose.click();
+    // await expect(searchInput).not.toBeVisible();
   });
 });
